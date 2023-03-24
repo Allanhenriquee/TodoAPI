de3b7b02cd9b8ebcd79764bb62fa08f55860f54d
Allan Henrique <allanhenriquegdacosta@hotmail.com>
2023-03-23 22:14:06 -0300

    adicionando um teste de unidade simples.

---
+++ test/TodoAPI.Tests/ToDoModelTests.cs
@@ -0,0 +1,22 @@
+﻿using ToDoAPI.Models;
+
+namespace ToDoAPI.Tests;
+
+public class ToDoModelTests
+{
+    [Fact]
+    public void CanChangeName()
+    {
+        // Arrange
+        var testTodo = new TodoItem
+        {
+            Name = "completed unit test"
+        };
+
+        // Act
+        testTodo.Name = "debugging";
+
+        // Assert
+        Assert.Equal("debugging", testTodo.Name);
+    }
+}
