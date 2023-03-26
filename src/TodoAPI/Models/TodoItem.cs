f0f79fa75189a72898403d3e7db530e295c8a984
Allan Henrique <allanhenriquegdacosta@hotmail.com>
2023-03-26 07:37:50 -0300

    Incluindo required na propriedade name.

--- src/TodoAPI/Models/TodoItem.cs
+++ src/TodoAPI/Models/TodoItem.cs
@@ -1,7 +1,10 @@
-﻿namespace ToDoAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models;
 
 public class TodoItem
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
 }
